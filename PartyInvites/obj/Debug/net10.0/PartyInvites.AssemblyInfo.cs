c93f3cfa2ce763f07a09d0a592f332bc4ad6b815
chaibaojie <chaibaojie@126.com>
2026-05-08 12:50:01 +0800

    完成

--- PartyInvites/obj/Debug/net10.0/PartyInvites.AssemblyInfo.cs
+++ PartyInvites/obj/Debug/net10.0/PartyInvites.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PartyInvites")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abf9d537009f9e6447bd6334aea300bcbff8f5e9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c696859bd0dbd45759860e00e00d8152bc7c2cb8")]
 [assembly: System.Reflection.AssemblyProductAttribute("PartyInvites")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PartyInvites")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
